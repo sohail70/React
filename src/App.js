70dc7ef92f11d81f7f77dda736b65619e0cb98b3
sohail70 <sohail.e.nia@gmail.com>
2022-11-20 19:29:37 +0330

    Do basic logic in jsx

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import './App.css';
 import Employee from './components/Employee'; // ./ means search in the same dir   --> u can use <Employee /> instead of <Employee> </Employee>
 
 function App() {
-  return ( 
+  console.log("We are about to list the employees");
+  const showEmployess = true; //ghable return mishe var ham sakht
+  return ( //chizi ke return mishe jsx hast ke shabih hamoon html hast --> albate logic ham mishe bargardond!
+  // vase inke code js bezani toye paeen bayad ono to {} bezari
+  //alabte deghat kun ke hamishe bayad single element bargardoni dar return pas kule tag haro bendaz to <> </> ya tag e div
     <div className="App">
-      <header className="App-header">
-        <Employee></Employee>   
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showEmployess ?
+      <>
+        <Employee></Employee> 
+        <Employee/>  
+        <Employee/>
+      </>
+      : <p>you are not authorized to see the employee</p>}
     </div>
   );
 }
