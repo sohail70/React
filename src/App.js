4afd2eb0457a82d304e1bab1b94d769f6f7b043d
sohail70 <sohail.e.nia@gmail.com>
2022-11-21 00:43:45 +0330

    Add state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Employee from './components/Employee'; // ./ means search in the same dir   --> u can use <Employee /> instead of <Employee> </Employee>
-
+import {useState} from 'react'; //chera Employee ro to {} nazashti chun toye employee export default karde bodim
 function App() {
+  //! number one rule of state is you never assign a value to the var directly you always go through set function like setRole in below
+  const [role,setRole] = useState('dev'); //role ye state hast na variable , setRole func hast va role ro set mikune - use set ham meghdare default migire ke dev dadim
+  let role2='dev';
   console.log("We are about to list the employees");
-  const showEmployess = true; //ghable return mishe var ham sakht
+  const showEmployess = true; //ghable return mishe var ham sakht - 
   return ( //chizi ke return mishe jsx hast ke shabih hamoon html hast --> albate logic ham mishe bargardond!
   // vase inke code js bezani toye paeen bayad ono to {} bezari
   //alabte deghat kun ke hamishe bayad single element bargardoni dar return pas kule tag haro bendaz to <> </> ya tag e div
@@ -12,9 +15,15 @@ function App() {
       {console.log("we are inside the return")} 
       {showEmployess ?
       <>
-        <Employee name="zart" role="intern"></Employee> 
-        <Employee name="abby" role="nurse"/>  
-        <Employee name="sam"/>
+        <input type="text" onChange={(e)=>{
+          console.log(e.target.value);
+          //role2 = e.target.value; ---> in javab nemide yani baes nemishe Employe dovom yani abby set beshe --> solution use kardan az state hast ke bala import kardim va ba useState ijadesh kardim va ba setRole set esh mikunim --> state chun be UI vasl mishe vali variable na
+          setRole(e.target.value);
+        }} 
+        />
+          <Employee name="zart" role="intern"></Employee> 
+          <Employee name="abby" role={role}/>  
+          <Employee name="sam"/>
       </>
       : <p>you are not authorized to see the employee</p>}
     </div>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -4,6 +4,9 @@
 function Employee(props){ //Employee is a template ---> not specific employee
     // dakhele html tag age bashi niazi be "" nist mesle Employee-- har var ro to {} mizari ama age dakhle {} bashi niazi be {} vase har var  nist ama vase string bayad "" bezari
     //deghat kun parent comp is responsible for data thats to be displayed --> props inside the child comp should not be changed---> yani value be props nade modfyesh nakun---> inkar dar parent comp yani App dar App.js anjam mishe
+    //! pas dar kul vazife in component disply kardan value hast va nachange kardanesh --> change ba parent hast ke ma dar App.js ba state inkaro kardim
+
+    // props.name = "spspsps"; // we are not meant to change the value of gthe props in the child --> error mide --> balke bayad dar parent az tarigh state taghiresh bedi --> state ba var fargh dare --> state can be tied to use interface --> state taghir kune UI uodate mishe automatic
     return( 
     <>    
     <h3>Employee {props.name}</h3>
