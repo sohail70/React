2cee5a934b82f50d64010a9b6461e14988b086df
sohail70 <sohail.e.nia@gmail.com>
2022-11-21 15:51:42 +0330

    Use the already existing button in Employee.js inside the EditEmployee.js and remember we installed react boostrap in the previous commit

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -10,9 +10,7 @@ function EditEmployee() {
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch static backdrop modal
-      </Button>
+      <button onClick={handleShow} className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Update</button>
 
       <Modal
         show={show}
