c6b0a09006336130fde6db7380587d0a8b112a15
sohail70 <sohail.e.nia@gmail.com>
2022-11-21 13:33:00 +0330

    Add loop and uuid to get rid of a warning about id

--- src/App.js
+++ src/App.js
@@ -1,10 +1,67 @@
 import './index.css';
 import Employee from './components/Employee'; // ./ means search in the same dir   --> u can use <Employee /> instead of <Employee> </Employee>
 import {useState} from 'react'; //chera Employee ro to {} nazashti chun toye employee export default karde bodim
+import {v4 as uuidv4} from 'uuid';
+
+
+
 function App() {
   //! number one rule of state is you never assign a value to the var directly you always go through set function like setRole in below
   const [role,setRole] = useState('dev'); //role ye state hast na variable , setRole func hast va role ro set mikune - use set ham meghdare default migire ke dev dadim
   let role2='dev';
+  const [employees,setEmployess] = useState(
+      [
+        {
+          id :1,
+          name: "Logan",
+          role:"Expert",
+          img:"https://hips.hearstapps.com/digitalspyuk.cdnds.net/16/21/1464175888-hugh-jackman-muscles-claws-in-the-wolverine.jpg?crop=0.628xw:1.00xh;0.194xw,0&resize=480:*",
+          alt: "Logan is missing", 
+        },
+
+        {
+          id: 2,
+          name: "X",
+          role:"Mentor",
+          img:"https://assets.entrepreneur.com/content/3x2/2000/20160901055636-ProfessorX.jpeg?auto=webp&quality=95&crop=16:9&width=675",
+          alt: "X is missing",
+        },
+
+        {
+          id:3,
+          name: "Dead pool",
+          role:"comedian",
+          img:"https://i.pinimg.com/564x/fd/bc/a8/fdbca800caa7f8deeee2554c7a5ea54e.jpg",
+          alt: "Dead pool is missing",
+        },
+
+        {
+          id:4,
+          name: "Magneto",
+          role:"Expert",
+          img:"https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/large/800/Magneto.X-Men-Series.webp",
+          alt: "Magneto is missing",
+        },
+
+        {
+          name: "Cyclops",
+          role:"Expert",
+          img:"https://grantland.com/wp-content/uploads/2014/04/cyclops-hp.gif?w=706",
+          alt: "Cyclops is missing",
+        },
+
+        {
+          name: "Jane",
+          role:"Expert",
+          img: "https://static.tvtropes.org/pmwiki/pub/images/jeangrey.jpg",
+          alt:  "Jane is missing",
+        },
+
+      ]
+  );
+  
+  
+  
   console.log("We are about to list the employees");
   const showEmployess = true; //ghable return mishe var ham sakht - 
   return ( //chizi ke return mishe jsx hast ke shabih hamoon html hast --> albate logic ham mishe bargardond!
@@ -23,13 +80,11 @@ function App() {
           />
 
           <div className='flex flex-wrap justify-center'>
-            <Employee name="Logan" role="Expert" img="https://hips.hearstapps.com/digitalspyuk.cdnds.net/16/21/1464175888-hugh-jackman-muscles-claws-in-the-wolverine.jpg?crop=0.628xw:1.00xh;0.194xw,0&resize=480:*" ></Employee> 
-            <Employee name="abby" role={role} img="https://hips.hearstapps.com/digitalspyuk.cdnds.net/16/21/1464175888-hugh-jackman-muscles-claws-in-the-wolverine.jpg?crop=0.628xw:1.00xh;0.194xw,0&resize=480:*"/>  
-            <Employee name="sam" img="https://hips.hearstapps.com/digitalspyuk.cdnds.net/16/21/1464175888-hugh-jackman-muscles-claws-in-the-wolverine.jpg?crop=0.628xw:1.00xh;0.194xw,0&resize=480:*"/>
-            <Employee name="sam" img="https://hips.hearstapps.com/digitalspyuk.cdnds.net/16/21/1464175888-hugh-jackman-muscles-claws-in-the-wolverine.jpg?crop=0.628xw:1.00xh;0.194xw,0&resize=480:*"/>
-            <Employee name="sam" img="https://hips.hearstapps.com/digitalspyuk.cdnds.net/16/21/1464175888-hugh-jackman-muscles-claws-in-the-wolverine.jpg?crop=0.628xw:1.00xh;0.194xw,0&resize=480:*"/>
-            <Employee name="sam" img="https://hips.hearstapps.com/digitalspyuk.cdnds.net/16/21/1464175888-hugh-jackman-muscles-claws-in-the-wolverine.jpg?crop=0.628xw:1.00xh;0.194xw,0&resize=480:*"/>
-            
+            {employees.map( (employee)=>{
+              console.log(employee);
+              console.log(uuidv4());
+                return (<Employee key={uuidv4()}  name ={employee.name} role ={employee.role} img ={employee.img} alt={employee.alt}/> ); //return age multiple line bod dar () bezaresh vagarna niazi nist
+              } ) }         
           </div>
       </>
       : <p>you are not authorized to see the employee</p>}
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -35,7 +35,7 @@ export default Employee; // to use Employee comp inside  other files
 function Employee(props){  
     return(
     <div class="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-        <img class="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt="an Employee face"/>
+        <img class="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt={props.alt}/>
         <div class="text-center space-y-2 sm:text-left">
         <div class="space-y-0.5">
             <p class="text-lg text-black font-semibold">
@@ -45,7 +45,7 @@ function Employee(props){
                 {props.role}
             </p>
         </div>
-        <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+        <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Update</button>
     </div>
   </div> );
 }
