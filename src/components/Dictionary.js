646d484d20cd4f232184254aa3bb2b537b418472
sohail70 <sohail.e.nia@gmail.com>
2022-11-26 16:24:00 +0330

    Use no dependency array

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -1,9 +1,12 @@
 import { useState , useEffect } from "react";
 export default function Dictionary(){
     const [word , setWord] = useState('');
-    
+    const [word2, setWord2] = useState('');
+
+
+
     useEffect(()=>{
-        console.log("State Updated",word);
+        console.log("State Updated",word+" "+word2);
     })
 
     return (
@@ -13,7 +16,15 @@ export default function Dictionary(){
                 //console.log paeen khoob nist va bayad dar useEffect bala bashse --> khoodet chek kun
                 // console.log("State Updated",word); //hanoz momkene set nashode bahse vase hamin khoob shayad log nakune --> ama age dar useEffect console bezari kar mikune chun pas az update shodan state kar mikune
             }} />
-            <h1>Let's get the defintion for {word}</h1>
+            <h2>Let's get the defintion for {word}</h2>
+
+
+            <input type="text" onChange={(e)=>{ 
+                setWord2(e.target.value); //mohem ---> setWord function is asynchronous so they are not guaranteed to have the result immediately after so the console below have problems
+                //console.log paeen khoob nist va bayad dar useEffect bala bashse --> khoodet chek kun
+                // console.log("State Updated",word); //hanoz momkene set nashode bahse vase hamin khoob shayad log nakune --> ama age dar useEffect console bezari kar mikune chun pas az update shodan state kar mikune
+            }} />
+            <h2>Let's get the defintion for {word2}</h2>
         </>
     )
 }
