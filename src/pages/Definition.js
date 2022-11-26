725052f29fb40aeeb3d9810d6928922bbb597a15
sohail70 <sohail.e.nia@gmail.com>
2022-11-26 18:35:34 +0330

    Handle 404 first part

--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -1,18 +1,26 @@
 
 import {useState, useEffect } from "react"
 import { v4 as uuidv4 } from 'uuid';
-import { useParams } from "react-router-dom";
+import { useParams , useNavigate} from "react-router-dom";
+
 
 export default function Definition(){
     const [word , setWord] = useState();
 
     //console.log(useParams());
     let {search} = useParams();
-
+    const navigate = useNavigate();
     useEffect(()=>{
         // fetch('https://api.dictionaryapi.dev/api/v2/entries/en/hello') //jaye hello mitoni ye vocab dg bezari
         fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+search) //jaye hello mitoni ye vocab dg bezari
-            .then((response) => response.json())
+            .then((response) => {
+                console.log(response.status);
+                if(response.status===404)
+                {
+                    navigate('/404');
+                }
+                return response.json();
+            })
             .then((data) => {
                 setWord(data[0].meanings);
                 //console.log(data[0].meanings);
