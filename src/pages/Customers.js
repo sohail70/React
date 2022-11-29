80731aa32364661b7d2198279361e36c165e1616
sohail70 <sohail.e.nia@gmail.com>
2022-11-29 19:45:07 +0330

    Get data from backend. remember to put forward slash at the end of the url in fetch in Customers.js

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,3 +1,15 @@
+import { useEffect , useState } from "react";
+
 export default function Customers(){
+    const [customers , setCustomers] = useState();
+    useEffect(()=>{
+        console.log("Fetching...");
+        fetch('http://localhost:8000/api/customers/') // forward slash akhar moheme
+        .then((response)=>{return response.json()}) //reponse is here -->mitoni {} va return ro ham nazari
+        .then((data)=>{ //data to assign to some state
+            console.log(data);
+            setCustomers(data);
+        }) 
+    },[]); //remmeber the empty dependency array
     return <h1>Hello There!</h1>;
 }
