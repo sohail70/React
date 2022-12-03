f941e803f4ab342525f51c026ef13ba3a8fee4fb
sohail70 <sohail.e.nia@gmail.com>
2022-12-03 20:09:32 +0330

    Handle 404 and 500  when getting data from backend

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,21 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useParams , useNavigate, Navigate } from "react-router-dom";
 import { useEffect , useState } from "react";
 import { Link } from "react-router-dom";
-
+import NotFound from "../components/NotFound";
 export default function Customer(){
     const {id} = useParams(); //useParam returns the object but we only want the id so we put it in {} to destructure that
     const [customer , setCustomer] = useState();
+    const [notFound , setNotFound] = useState();
+    const navigate = useNavigate();
+
     useEffect(()=>{
         console.log('useEffect'); //dobar useEffect ro mibini chun strict mode et active hast --> felan bezar bashe chun dar dev mode hastim
         const url = 'http://localhost:8000/api/customers/' + id;
         fetch(url)
-        .then((response)=>{return response.json()})
+        .then((response)=>{
+            if(response.status===404)
+            {
+                // navigate('/404'); //ravesh 1:redirect to a 404 page (new URL)
+                setNotFound(true); //ravesh 2: render a 404 compoent in this page
+
+            }
+            return response.json()
+        })
         .then((data)=>{
             setCustomer(data.customer); //data e ma dar backend chanta property dare vase hamiin az dot use kardim
         })
     } , []);
     return (
         <>
+            {notFound? <><NotFound/> <p>The customer with id {id} was not found </p></>: null}
             {customer ? 
             <div>
                 <p>{customer.id}</p>
