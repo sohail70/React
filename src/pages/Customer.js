9fe6a8608866aa1cd6075aad3811b582d53ad906
sohail70 <sohail.e.nia@gmail.com>
2022-12-04 21:18:54 +0330

    Add ability to edit the DB from front end but there is still one step more to go

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -8,8 +8,18 @@ import { baseUrl } from "../shared";
 export default function Customer(){
     const {id} = useParams(); //useParam returns the object but we only want the id so we put it in {} to destructure that
     const [customer , setCustomer] = useState();
+    const [tempCustomer , setTempCustomer] = useState();
     const [notFound , setNotFound] = useState();
+    const [changed , setChanged] = useState(false);
+    
     const navigate = useNavigate();
+    
+
+    useEffect(()=>{ // ta vaghti update mikunim input haye paeen dar return ro ye chizi benvise --> tempCustomer ye state ast va useEffect state change ro mifahme
+        console.log('customer' , customer);
+        console.log('temp customer' , tempCustomer); 
+        console.log(changed);
+    });
 
     useEffect(()=>{
         console.log('useEffect'); //dobar useEffect ro mibini chun strict mode et active hast --> felan bezar bashe chun dar dev mode hastim
@@ -22,24 +32,45 @@ export default function Customer(){
                 setNotFound(true); //ravesh 2: render a 404 compoent in this page
 
             }
-            return response.json()
+            return response.json();
         })
         .then((data)=>{
             setCustomer(data.customer); //data e ma dar backend chanta property dare vase hamiin az dot use kardim
+            setTempCustomer(data.customer);
         })
     } , []);
 
 
-    
+   function updateCustomer()
+   {
+       const url = baseUrl + 'api/customers/' + id;
+       fetch(url , {method: 'POST' , headers: {'Content-Type': 'application/json'}  , body:JSON.stringify(tempCustomer)})
+       .then((response)=>{return response.json()})
+       .then((data)=>{
+            setChanged(false); //vaghti successful shod false mikunim ta button ha hazf beshe bad az save kardan
+            console.log(data); //updated object from the data base
+       })
+   } 
 
     return (
         <>
             {notFound? <><NotFound/> <p>The customer with id {id} was not found </p></>: null}
             {customer ? 
             <div>
-                <p>{customer.id}</p>
-                <p>{customer.name}</p>
-                <p>{customer.industry}</p>
+                <p className= "m-2 block px-2" type="text">ID: {tempCustomer.id}</p> 
+                <input className= "m-2 block px-2" type="text" value={tempCustomer.name} onChange={(e)=>{ //e is the event
+                    setChanged(true);
+                    setTempCustomer({...tempCustomer , name:e.target.value}) ; //...temCustomer ke id ro mizere az ghabl va e.target.value har chi to input gozashti ro minvise
+                }}/>
+                <input className= "m-2 block px-2" type="text" value={tempCustomer.industry} onChange={(e)=>{
+                    setChanged(true);
+                    setTempCustomer({...tempCustomer , industry:e.target.value}) ;
+                }}/>
+                {changed ? <><button onClick={(e)=>{
+                    setTempCustomer({...customer}); //agar cancel ro bezani bargarde be halate avalie
+                    setChanged(false);
+                    }}>Cancel</button> {' '}
+                    <button onClick={updateCustomer}>Save</button> </>: null}
             </div> 
             
                     : null}
